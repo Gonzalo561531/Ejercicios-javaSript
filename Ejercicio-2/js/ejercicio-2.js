e98d8a865f515ca4403bceabfe1f5b52adc97dab
Gonzalo561531 <miki.gonzalo56@gmail.com>
2018-12-14 10:49:10 -0500

    ejercicio-2
    /*utilizando un bucle,mostrar la suma y la media de los numeros
    intruducidos hasta intruducir un numero negativo y ahi mostrar el resulatado*/

---
+++ Ejercicio-2/js/ejercicio-2.js
@@ -0,0 +1,22 @@
+'use strict'
+
+/*utilizando un bucle,mostrar la suma y la media de los numeros
+intruducidos hasta intruducir un numero negativo y ahi mostrar el resulatado*/
+
+var suma = 0;
+var contador = 0;
+
+do{
+    var numero = parseInt(prompt("ingrese un numero hasta que introdusca uno negativo",0));
+    if (isNaN(numero)) {
+    	numero = 0;
+    }else if(numero>=0){
+    	suma += numero;
+    	contador++;
+    }
+    console.log(suma);
+    console.log(contador);
+}while (numero >= 0)
+
+alert("la suma de todos los numeros es: "+suma);
+alert("la media de todos los numeros es: "+(suma/contador));
